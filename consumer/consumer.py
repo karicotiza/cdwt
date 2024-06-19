bb296772f393285799091c90e96d027c9d3b3ed8
Danial Kurtumerov <kur7umerovd@gmail.com>
2024-06-19 12:57:58 +0500

    feat: consumer
    Added consumer file.

---
+++ consumer/consumer.py
@@ -0,0 +1,27 @@
+from os import environ
+from celery import Celery  # type: ignore
+from celery.exceptions import SoftTimeLimitExceeded  # type: ignore
+
+
+user: str = environ.get('RABBITMQ_DEFAULT_USER', '')
+password: str = environ.get('RABBITMQ_DEFAULT_PASS', '')
+vhost: str = environ.get('RABBITMQ_DEFAULT_VHOST', '')
+broker: str = f'pyamqp://{user}:{password}@rabbitmq:5672/{vhost}'
+redis_password = environ.get('REDIS_DEFAULT_PASS', '')
+backend: str = f'redis://:{redis_password}@redis:6379'
+
+app = Celery(
+    'tasks',
+    broker=broker,
+    backend=backend,
+    broker_connection_retry_on_startup=True,
+    task_soft_time_limit=2,
+)
+
+
+@app.task
+def addition(first: int, second: int) -> int:
+    try:
+        return first + second
+    except SoftTimeLimitExceeded:
+        return -1
